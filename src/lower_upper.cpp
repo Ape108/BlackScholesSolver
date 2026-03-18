f930afc4eeb87fd5eb3df4b06ffac27641d17561
Cameron Akhtar <cameronakhtar.kc@gmail.com>
2026-03-18 20:23:48 +0000

    format

--- src/lower_upper.cpp
+++ src/lower_upper.cpp
@@ -1,12 +1,13 @@
 #include "../include/lower_upper.hpp"
 
 Decomposed lu_decomposition(const std::vector<float>& a, const std::vector<float>& b, const std::vector<float>& c) {
+    
     Decomposed LU;
-    LU.upper.push_back(a[0]); // Set u_1 <--- a_1
+    LU.upper.push_back(a[0]);
     std::size_t n = a.size();
 
     for (std::size_t i=1; i<n; i++) {
-        float l_i = c[i-1] / LU.upper[i-1]; // l_i = c_i-1 / u_i-1
+        float l_i = c[i-1] / LU.upper[i-1];
         LU.lower.push_back(l_i);
         float u_i = a[i] - (l_i * b[i-1]);
         LU.upper.push_back(u_i); 
